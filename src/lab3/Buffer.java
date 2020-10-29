31610a181c15abd2c25c37f2cdce47643331f7d9
Kamil Krzempek <kamilkrzempek99@gmail.com>
2020-10-29 18:12:53 +0100

    fourth lab - producer consumer program fixed (and messed up again)

--- src/lab3/Buffer.java
+++ src/lab3/Buffer.java
@@ -2,34 +2,53 @@ package lab3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Buffer {
     private List<Integer> units;
     private int maxUnits;
+    Lock lock = new ReentrantLock();
+    Condition isEmpty = lock.newCondition();
+    Condition isFull = lock.newCondition();
 
     public Buffer(int size) {
         this.units = new ArrayList<>();
         this.maxUnits = size;
     }
 
-    synchronized void produce(int unit) throws InterruptedException {
-        while (this.units.size() >= this.maxUnits) {
-            System.out.println("Produces waits with value " + unit);
-            wait();
+    void produce(List<Integer> newUnits) throws InterruptedException {
+        lock.lock();
+        try {
+            while (this.units.size() > this.maxUnits - newUnits.size()) {
+                System.out.println("Produces waits with " + newUnits.size() + " units");
+                isFull.await();
+            }
+            this.units.addAll(newUnits);
+            System.out.println("Produced " + newUnits.size() + " units");
+            isEmpty.signal();
+        } finally {
+            lock.unlock();
         }
-        this.units.add(unit);
-        System.out.println("Produced unit of value " + unit);
-        notify();
     }
 
-    synchronized int consume() throws InterruptedException {
-        while (this.units.isEmpty()) {
-            System.out.println("Consumer waits");
-            wait();
+    List<Integer> consume(int unitsCount) throws InterruptedException {
+        lock.lock();
+        try {
+            while (this.units.size() < unitsCount) {
+                System.out.println("Consumer waits for " + unitsCount + " units");
+                isEmpty.await();
+            }
+            List<Integer> newUnits = new ArrayList<>();
+            for (int i = 0; i < unitsCount; i++) {
+                newUnits.add(this.units.remove(0));
+            }
+            System.out.println("Consumed " + unitsCount + " units");
+            isFull.signal();
+            return newUnits;
+        } finally {
+            lock.unlock();
         }
-        int unit = this.units.remove(0);
-        System.out.println("Consumed unit of value " + unit);
-        notify();
-        return unit;
     }
 }
--- src/lab3/ProducersConsumers.java
+++ src/lab3/ProducersConsumers.java
@@ -1,12 +1,18 @@
 package lab3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ProducersConsumers {
     private static int PRODUCER_COUNT = 1;
-    private static int CONSUMER_COUNT = 1;
-    private static int BUFFER_SIZE = 3;
+    private static int CONSUMER_COUNT = 2;
+    private static int BUFFER_SIZE = 10;
+
+    public static int randomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
 
     public static void main(String[] args) {
         Buffer buffer = new Buffer(BUFFER_SIZE);
@@ -14,8 +20,10 @@ public class ProducersConsumers {
         Runnable producerRunnable = () -> {
             while (true) {
                 try {
-                    int unit = (int) (Math.random() * 1000);
-                    buffer.produce(unit);
+                    int unitsCount = randomNumber(1, BUFFER_SIZE);
+                    List<Integer> units = new ArrayList<>();
+                    for (int i = 0; i < unitsCount; i++) units.add(randomNumber(0, 1000));
+                    buffer.produce(units);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -25,7 +33,7 @@ public class ProducersConsumers {
         Runnable consumerRunnable = () -> {
             while (true) {
                 try {
-                    buffer.consume();
+                    buffer.consume(randomNumber(1, BUFFER_SIZE));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
